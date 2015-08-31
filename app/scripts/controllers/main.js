9918422165f965754d934ed98cb5c906e0484703
Alouise Quiatchon <aquiatchon@shutterfly.com>
2015-08-31 13:56:35 -0700

    fixed y labels for home page:sfly

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -15,11 +15,11 @@ define(['angular'], function (angular) {
 
             var wTime = 900;
             var wSLA = 700;
-            var h = 250;
+            var hSFLY = 250;
             var hSLA = 150;
             var fontSize = 12;
             var padding = 10;
-            var percentLabelOffset = 20 + fontSize;
+            var labelOffset = 20 + fontSize;
             var yOffset = 3;
             var yScaleSLA = 1;
             var yScaleTime = 8;
@@ -28,7 +28,8 @@ define(['angular'], function (angular) {
             var dataset = [];
             var times = [];
             var percentLabels = [0, 25, 50, 75, 100];
-            var timeLabels = [0, 5, 10, 15, 20, 25];
+            var timeLabels = [0, 6, 12, 18, 24];
+            // var timeLabels = [0, 5, 10, 15, 20, 25];
             var timeseriesSFLY = [
                 ['12am', 5],
                 ['1am', 2],
@@ -72,7 +73,7 @@ define(['angular'], function (angular) {
             
             var svgSLA = d3.select('.sla')
                 .append('svg')
-                    .attr('width', wSLA + percentLabelOffset)
+                    .attr('width', wSLA + labelOffset)
                     .attr('height', hSLA);
 
             // Add bars
@@ -81,7 +82,7 @@ define(['angular'], function (angular) {
                 .enter()
                 .append('rect')
                     .attr('x', function (d, i) {    // d == data; i == index
-                        return i * (wSLA / dataSLA.length) + percentLabelOffset;
+                        return i * (wSLA / dataSLA.length) + labelOffset;
                     })
                     .attr('y', function (d) {
                         return hSLA - (d[1] * yScaleSLA) - fontSize - yOffset;
@@ -106,7 +107,7 @@ define(['angular'], function (angular) {
                 .text(function (d) { return d[0]; })
                 .attr({
                     'text-anchor': 'middle',
-                    x: function (d, i) { return i * (wSLA / dataSLA.length) + ((wSLA / dataSLA.length - padding) / 2) + percentLabelOffset; },
+                    x: function (d, i) { return i * (wSLA / dataSLA.length) + ((wSLA / dataSLA.length - padding) / 2) + labelOffset; },
                     y: function () { return hSLA - yOffset; },
                     'font-family': "sans-serif",
                     'font-size': fontSize
@@ -131,8 +132,8 @@ define(['angular'], function (angular) {
 
             var svgSFLY = d3.select('.sfly')
                 .append('svg')
-                    .attr('width', wTime)
-                    .attr('height', h);
+                    .attr('width', wTime + labelOffset)
+                    .attr('height', hSFLY);
 
             // Add bars
             svgSFLY.selectAll('rect')
@@ -140,10 +141,10 @@ define(['angular'], function (angular) {
                 .enter()
                 .append('rect')
                     .attr('x', function (d, i) {    // d == data; i == index
-                        return i * (wTime / timeseriesSFLY.length);
+                        return i * (wTime / timeseriesSFLY.length) + labelOffset;
                     })
                     .attr('y', function (d) {
-                        return h - (d[1] * yScaleTime) - fontSize - yOffset;
+                        return hSFLY - (d[1] * yScaleTime) - fontSize - yOffset;
                     })
                     .attr('width', wTime / timeseriesSFLY.length - padding)
                     .attr('height', function (d) {
@@ -158,15 +159,31 @@ define(['angular'], function (angular) {
                     });
 
             // Add x labels
-            svgSFLY.selectAll('text')
+            svgSFLY.selectAll('text.x')
                 .data(timeseriesSFLY)
                 .enter()
                 .append('text')
                 .text(function (d) { return d[0]; })
                 .attr({
                     'text-anchor': 'middle',
-                    x: function (d, i) { return i * (wTime / timeseriesSFLY.length) + (wTime / timeseriesSFLY.length - padding) / 2; },
-                    y: function () { return h - yOffset; },
+                    x: function (d, i) { return i * (wTime / timeseriesSFLY.length) + ((wTime / timeseriesSFLY.length - padding) / 2) + labelOffset; },
+                    y: function () { return hSFLY - yOffset; },
+                    'font-family': "sans-serif",
+                    'font-size': fontSize
+                });
+
+            // Add y labels
+            svgSFLY.selectAll('text.y')
+                .data(timeLabels)
+                .enter()
+                .append('text')
+                .text(function (d) { return d + 's'; })
+                .attr({
+                    'text-anchor': 'middle',
+                    x: fontSize,
+                    y: function (d, i) {
+                        var scaledY = d * yScaleTime; 
+                        return hSFLY - scaledY - fontSize; },
                     'font-family': "sans-serif",
                     'font-size': fontSize
                 });
